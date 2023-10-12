bca5e4dd8511fd4c8370592b809d90c23f7aceab
liuchenhui <liu.ch@yz-fund.com.cn>
2023-10-12 13:54:09 +0800

    初步实现

--- myrequests/core.py
+++ myrequests/core.py
@@ -7,23 +7,39 @@
 """
 import urllib
 import urllib.request
-import urllib2
+import urllib.parse
 
 __title__ = 'requests'
-__version__ = '0.0.1'
-__build__ = 0x000001
+__version__ = '0.2.0'
+__build__ = 0x000200
 __author__ = 'caleb'
 __license__ = ''
 __copyright__ = 'Copyright ...'
 
 AUTOAUTHS = []
 
-
-class Request(object):
-    """The :class: `Request` object. It's awesome.
+class _Request(urllib.request.Request):
+    """Hidden wrapper around the urllib.request.Request object. Allows for manual
+        setting of HTTP methods.
     """
 
-    _METHODS = ('get', 'head', 'put', 'post', 'delete')
+    def __init__(self, url, data=None, headers={},
+                    origin_req_host=None, unverifiable=False, method=None):
+        urllib.request.Request.__init__(self, url, data, headers, origin_req_host, unverifiable)
+        self.method = method
+    
+    def get_method(self):
+        if self.method:
+            return self.method
+        
+        return urllib.request.Request.get_method(self)
+
+class Request(object):
+    """The :class: `Request` object. It carries out all functionality of
+        Requests. Recommended interface is with the Requests functions.
+    """
+
+    _METHODS = ('GET', 'HEAD', 'PUT', 'POST', 'DELETE')
 
     def __init__(self):
         self.url = None
@@ -31,67 +47,132 @@ class Request(object):
         self.method = None
         self.params = {}
         self.data = {}
-        self.reponse = Response()
+        self.response = Response()
         self.auth = None
         self.sent = False
 
     def __setattr__(self, name, value):
         if (name == 'method') and (value):
-            if not value.lower() in self._METHODS:
-                raise InvalidMethods()
+            if not value in self._METHODS:
+                raise InvalidMethod()
         
         object.__setattr__(self, name, value)
 
     def _checks(self):
-        pass
+        """Deterministic checks for consistiency. """
+        
+        if not self.url:
+            raise URLRequired
+
+    def _get_opener(self):
+        """ Createa appropriate opener object for urllib.request
+        """
+        if self.auth:
+
+            # create a password manager
+            authr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+
+            authr.add_password(None, self.url, self.auth.username, self.auth.password)
+            handler = urllib.request.HTTPBasicAuthHandler(authr)
+            opener = urllib.request.build_opener(handler)
+
+            # use the opener to fetch a URL
+            return opener.open
+        else:
+            return urllib.request.urlopen
     
     def send(self, anyway=False):
-        """Sends the requests.
+        """Sends the requests. Returns True if successful, false if not.
+            If there was an HTTPError during transmission,
+            self.response.status_code will contain the HTTPError code.
+
+            Once a request is successfully sent, `sent` will equal True.
 
             :param anyway: If True, request will be sent, even if it has already been sent.
         """
         self._checks()
-        if self.method.lower() == 'get':
+
+        success = False
+
+        if self.method in ('GET', 'HEAD', 'DELETE'):
             if (not self.sent) or anyway:
+
+                # url encode GET params if it's a dict
+                if isinstance(self.params, dict):
+                    params = urllib.parse.urlencode(self.params)
+                else:
+                    params = self.params
+                
+                req = _Request(("%s?%s" % (self.url, params)), method=self.method)
+                
+                if self.headers:
+                    req.headers = self.headers
+                
+                opener = self._get_opener()
+
                 try:
-                    r = urllib.request.urlopen('http://www.baidu.com')
-                    self.response.headers = r.headers.dict
-                    self.response.status_code = r.code
-                    self.response.content = r.read()
+                    resp = opener(req)
+                    
+                    self.response.headers = resp.headers
+                    self.response.status_code = resp.code
+                    if self.method == 'GET':
+                        self.response.content = resp.read()
 
                     success = True
                 
-                except Exception:
-                    raise RequestException
+                except urllib.request.HTTPError:
+                    # self.response.status_code = why.code
+                    print("HTTPError")
 
-        elif self.method.lower() == 'head':
+        elif self.method == 'PUT':
             if (not self.sent) or anyway:
-                try:
-                    pass 
+
+                req = _Request(self.url, method='PUT')
+                req.data = self.data
+
+                if self.headers:
+                    req.headers = self.headers
+
+                try:       
+                    opener = self._get_opener()
+                    resp = opener(req)
+
+                    self.response.status_code = resp.code
+                    self.response.headers = resp.headers
+                    self.reponse.content = resp.read()
+                    
                     success = True
-                except Exception:
-                    raise RequestException
-        elif self.method.lower() == 'put':
+                except (urllib.request.HTTPError, why):
+                    self.reponse.status_code = why.code
+        
+        elif self.method == 'POST':
             if (not self.sent) or anyway:
-                try:
-                    pass
+
+                req = _Request(self.url, method='POST')
+                
+                if self.headers:
+                    req.headers = self.headers
+                
+                # url encode form data if it's a dict
+                if isinstance(self.data, dict):
+                    req.data = urllib.parse.urlencode(self.data)
+                else:
+                    req.data = self.data
+
+                try: 
+                    opener = self._get_opener()
+
+                    resp = opener(req)
+                    
+                    self.reponse.status_code = resp.code
+                    self.response.headers = resp.headers
+                    self.response.content = resp.read()
+
                     success = True
-                except:
-                    raise RequestException
-        elif self.method.lower() == 'post':
-            if (not self.sent) or anyway:
-                try:
-                    req = 
-                    success = True
-                except Exception:
-                    raise RequestException
-        elif self.method.lower() == 'delete':
-            if (not self.sent) or anyway:
-                try:
-                    pass
-                    success = True
-                except Exception:
-                    raise RequestException
+                
+                except (urllib.request.HTTPError, why):
+                    self.reponse.status_code = why.code
+        
         else:
             raise InvalidMethod
         
@@ -100,7 +181,9 @@ class Request(object):
         return success
 
 class Response(object):
-    """The :class:`Request` object. It's awesome.
+    """The :class:`Request` object. All :class:`Request` objects contain a
+        :class: `Request.response <response>` attribute, which is an instance of
+        this class.
     """
 
     def __init__(self):
@@ -108,8 +191,12 @@ class Response(object):
         self.status_code = None
         self.headers = dict()
 
+
 class AuthObject(object):
-    """The :class: `AuthObject` is a simple HTTP Authentication token.
+    """The :class: `AuthObject` is a simple HTTP Authentication token. When
+        given to a Requests function, it enables Basic HTTP Authentication for that
+        Request. You can also enable Authorization for domain realms with AutoAuth.
+        See AuthAuth for more details.
 
     :param username: Username to authenticate with.
     :param password: Password for given username.
@@ -121,33 +208,54 @@ class AuthObject(object):
     
 def get(url, params={}, headers={}, auth=None):
     """Sends a GET requests. Returns :class:`Response` object.
+
+    :param url: URL for the new :class:`Request` object.
+	:param params: (optional) Dictionary of GET Parameters to send with the :class:`Request`.
+	:param headers: (optional) Dictionary of HTTP Headers to sent with the :class:`Request`.
+	:param auth: (optional) AuthObject to enable Basic HTTP Auth.
     """
     r = Request()
 
     r.method = 'GET'
     r.url = url 
     r.headers = headers
+    r.params = params
     r.auth = _detect_auth(url, auth)
+
     r.send()
 
     return r.response
 
 def head(url, params={}, headers={}, auth=None):
     """Sends a HEAD request. Returns :class:`Response` object
+    :param url: URL for the new :class:`Request` object.
+	:param params: (optional) Dictionary of GET Parameters to send with the :class:`Request`.
+	:param headers: (optional) Dictionary of HTTP Headers to sent with the :class:`Request`.
+	:param auth: (optional) AuthObject to enable Basic HTTP Auth.
     """
     r = Request()
+
     r.method = 'HEAD'
     r.url = url
     r.headers = headers
+    r.params = params
+
     r.auth = _detect_auth(url, auth)
+
     r.send()
 
     return r.response
 
-def post(url, params={}, headers={}, auth=None):
+def post(url, data={}, headers={}, auth=None):
     """Sends a POST request. Returns :class:`Response` object
+
+    :param url: URL for the new :class:`Request` object.
+	:param data: (optional) Dictionary of POST Data to send with the :class:`Request`.
+	:param headers: (optional) Dictionary of HTTP Headers to sent with the :class:`Request`.
+	:param auth: (optional) AuthObject to enable Basic HTTP Auth.
     """
     r = Request()
+
     r.method = 'POST'
     r.url = url
     r.headers = headers
@@ -159,19 +267,32 @@ def post(url, params={}, headers={}, auth=None):
 
 def put(url, data='', headers={}, auth=None):
     """Sends a PUT requests. Returns :class:`Response` object
+
+    :param url: URL for the new :class:`Request` object.
+	:param data: (optional) Bytes of PUT Data to send with the :class:`Request`.
+	:param headers: (optional) Dictionary of HTTP Headers to sent with the :class:`Request`.
+	:param auth: (optional) AuthObject to enable Basic HTTP Auth.
     """
     r = Request()
 
     r.url = url
     r.method = 'PUT'
     r.headers = headers
+    r.data = data
+
     r.auth = _detect_auth(url, auth)
+
     r.send()
 
     return r.response()
 
 def delete(url, params={}, headers={}, auth=None):
     """Sends a DELETE request. Returns :class:`Response` object
+
+    :param url: URL for the new :class:`Request` object.
+	:param params: (optional) Dictionary of GET Parameters to send with the :class:`Request`.
+	:param headers: (optional) Dictionary of HTTP Headers to sent with the :class:`Request`.
+	:param auth: (optional) AuthObject to enable Basic HTTP Auth.
     """
     r = Request()
 
@@ -185,22 +306,44 @@ def delete(url, params={}, headers={}, auth=None):
     return r.response()
 
 def add_autoauth(url, authobject):
-    global AUTHOAUTHS
+    """Registers given AuthObject to given URL domain. for auto-activation.
+	Once a URL is registered with an AuthObject, the configured HTTP
+	Authentication will be used for all requests with URLS containing the given
+	URL string.
 
-    AUTHOAUTHS.append((url, authobject))
+	Example: ::
+	    >>> c_auth = requests.AuthObject('kennethreitz', 'xxxxxxx')
+	    >>> requests.add_autoauth('https://convore.com/api/', c_auth)
+	    >>> r = requests.get('https://convore.com/api/account/verify.json')
+	    # Automatically HTTP Authenticated! Wh00t!
+
+	:param url: Base URL for given AuthObject to auto-activate for.
+	:param authobject: AuthObject to auto-activate.
+	"""
+
+    global AUTOAUTHS
+
+    AUTOAUTHS.append((url, authobject))
 
 def _detect_auth(url, auth):
+    """Returns registered AuthObject for given url if available, defaulting to
+	given AuthObject."""
+
     return _get_autoauth(url) if not auth else auth
 
 def _get_autoauth(url):
-    for (authauth_url, auth) in AUTHOAUTHS:
-        if authauth_url in url:
+    """Returns registered AuthObject for given url if available.
+	"""
+
+    for (autoauth_url, auth) in AUTOAUTHS:
+        if autoauth_url in url:
             return auth
+    
     return None
 
 
-class InvalidMethod(Exception):
-    """An innappropriate method was attemped."""
+class RequestException(Exception):
+	"""There was an ambiguous exception that occured while handling your request."""
 
 class AuthenticationError(RequestException):
     """The authentication credentials provided were invalid."""
--- test_requests.py
+++ test_requests.py
@@ -1,8 +1,36 @@
 
 import unittest
 
-import myrequests 
+import myrequests.core 
 
-print(myrequests.get('http://www.baidu.com')).headers
+class MyRequestsTestSuite(unittest.TestCase):
+    """Requests test cases."""
 
-r = myrequests.Request()
+    def setUp(self):
+        pass 
+    
+    def tearDown(self):
+        """Testdown."""
+        pass
+
+    def test_invalid_url(self):
+        self.assertRaises(ValueError, myrequests.core.get, 'hiwpefhipowhefopw')
+
+    def test_HTTP_200_OK_GET(self):
+        r = myrequests.core.get('http://www.baidu.com')
+        self.assertEqual(r.status_code, 200)
+    
+    def test_HTTPS_200_OK_GET(self):
+        r = myrequests.core.get('https://www.baidu.com')
+        self.assertEqual(r.status_code, 200)
+    
+    def test_HTTP_200_OK_HEAD(self):
+        r = myrequests.core.head('http://www.baidu.com')
+        self.assertEqual(r.status_code, 200)
+    
+    def test_HTTPS_200_OK_HEAD(self):
+        r = myrequests.core.head('https://www.baidu.com')
+        self.assertEqual(r.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
